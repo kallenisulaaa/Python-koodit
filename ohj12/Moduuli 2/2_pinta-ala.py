0c3804bd0be2bc6377323c3f1ebefb7c6195981a
kalle <kalle.nisula@metropolia.fi>
2024-08-21 13:37:06 +0300

    Kolmas tehtävä moduuli 2:n

---
+++ ohj12/Moduuli 2/2_pinta-ala.py
@@ -0,0 +1,7 @@
+kanta = int(input("Syötä suorakulmion kanta:\n"))
+korkeus = int(input("Syötä suorakulmion korkeus:\n"))
+
+pa = kanta * korkeus
+piiri = kanta + kanta + korkeus + korkeus
+
+print("Suorakulmion pinta-ala on",pa,"ja piiri on",piiri)
