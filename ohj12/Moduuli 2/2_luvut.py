6d500d88de3104cf54466c0508d5f68dccbd38bc
kalle <kalle.nisula@metropolia.fi>
2024-08-21 13:48:06 +0300

    Neljäs tehtävä moduuli 2:n

---
+++ ohj12/Moduuli 2/2_luvut.py
@@ -0,0 +1,7 @@
+yksi = int(input("Ensimmäinen luku: \n"))
+kaksi = int(input("Toinen luku: \n"))
+kolme = int(input("Kolmas luku: \n"))
+
+print(yksi+kaksi+kolme)
+print(yksi*kaksi*kolme)
+print(float(yksi+kaksi+kolme)/3)
