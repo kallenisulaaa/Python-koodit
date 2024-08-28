bcd59bc0fc147624bf056310f3340fa9c2c65fd3
kalle <kalle.nisula@metropolia.fi>
2024-08-28 13:36:06 +0300

    Ensimmäinen tehtävä Moduuli 3:en. Typo korjattu.

--- ohj12/Moduuli 3/1_kuha.py
+++ ohj12/Moduuli 3/1_kuha.py
@@ -3,4 +3,4 @@ pituus = int(input("Anna kuhan pituus:\n"))
 if pituus >= 37:
     print("Hyvä saalis!")
 if pituus < 37:
-    print("Laske kala takaisin järveen! Saaliisi on", 37 - pituus,"cm alle sallitusta pyyntimitasti.")
+    print("Laske kala takaisin järveen! Saaliisi on", 37 - pituus,"cm alle sallitusta pyyntimitasta.")
