7c899c1b1252eaabac85cfb45a863e98f166ead0
kalle <kalle.nisula@metropolia.fi>
2024-09-09 00:22:41 +0300

    Neljäs tehtävä Moduuli 6:n.

---
+++ ohj12/Moduuli 6/4_lista.py
@@ -0,0 +1,7 @@
+def laske_summa(lista):
+    return sum(lista)
+
+if __name__ == '__main__':
+    luvut = [1, 2, 3, 4, 5]
+    summa = laske_summa(luvut)
+    print(f"Listan lukujen summa on: {summa}")
